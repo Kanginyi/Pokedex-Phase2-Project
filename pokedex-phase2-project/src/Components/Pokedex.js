bfc97b870df150bab035108b13e3f9e6433db420
Eric Yi <erickanginyi@gmail.com>
2021-10-04 12:27:33 -0400

    Import Pokemon & render onto page

--- pokedex-phase2-project/src/Components/Pokedex.js
+++ pokedex-phase2-project/src/Components/Pokedex.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Pokemon from './Pokemon';
 
 function Pokedex() {
+    const [pokemonData, setPokemonData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:4000/pokemons")
+            .then(resp => resp.json())
+            .then(data => setPokemonData(data));
+    }, [])
+
+    console.log(pokemonData);
+
+    const renderPokemon = pokemonData.map(pokemon => {
+        return <Pokemon key={pokemon.id} name={pokemon.name.eng} id={pokemon.id} />
+    })
+
     return (
-        <h1>hi</h1>
+        <div>
+            {renderPokemon}
+        </div>
     );
 }
 
 export default Pokedex;
+
+//Ask about where to put this db.json file to better access it
+
+// {
+//     "id": "001",
+//     "name": {
+//       "eng": "Bulbasaur",
+//       "jpn": "フシギダネ",
+//       "jpn_ro": "Fushigidane"
+//     },
+//     "slug": {
+//       "eng": "bulbasaur",
+//       "jpn": "fushigidane"
+//     },
+//     "icons": {
+//       "_": {}
+//     }
+//   },
