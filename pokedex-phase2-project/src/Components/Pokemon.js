3649f1ceafd6e3e394eaab865985457ce064c810
Eric Yi <erickanginyi@gmail.com>
2021-10-04 13:49:04 -0400

    Update placeholder image to Pokeball

--- pokedex-phase2-project/src/Components/Pokemon.js
+++ pokedex-phase2-project/src/Components/Pokemon.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-function Pokemon({id, name}) {
+function Pokemon({id, name, image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/240px-Pok%C3%A9_Ball_icon.svg.png"}) {
     return (
         <> 
             <h1>{id}</h1>
             <h2>{name}</h2>
+            <img
+                src={image}
+                alt={`Pokemon ` + id + `: ` + name}
+                title={`Pokemon ` + id + `: ` + name}
+                width="250px"
+                height="250px"
+            />
         </>
     );
 }
