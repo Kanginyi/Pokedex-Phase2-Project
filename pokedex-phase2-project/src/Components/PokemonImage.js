a616f1b8865a04a4843017dea91d86f85a639892
QuangDo <qdangdo@gmail.com>
2021-10-08 02:23:12 -0500

    Move image up and down

--- pokedex-phase2-project/src/Components/PokemonImage.js
+++ pokedex-phase2-project/src/Components/PokemonImage.js
@@ -6,25 +6,27 @@ const PokemonImage = ({pokeID}) => {
 
 
     return (
-        <>
-            <PokeImage alt="Pokemon" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`}/>
-        </>
+        <PokeImage>
+            <img alt="Pokemon" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeID}.png`}/>
+        </PokeImage>
     )
 }
 
 export default PokemonImage
 
-// const spriteMovement = keyframes`
-//     0% {transform: translateY(0px)};
-//     25% {transform: translateY(-8px)};
-//     50% {transform: translateY(0px)};
-//     75% {transform: translateY(-4px)};
-//     100% {transform: translateY(0px)};
-// `
+const spriteMovement = keyframes`
+    0% {transform: translateY(0px)};
+    25% {transform: translateY(-50px)};
+    50% {transform: translateY(0px)};
+    75% {transform: translateY(-4px)};
+    100% {transform: translateY(0px)};
+`
 
-const PokeImage = styled.img`
+const PokeImage = styled.div`
+    & img {
     animation-name: ${spriteMovement};
     animation-duration: 0.5s;
     animation-iteration-count: 1;
+    }
 `
 
