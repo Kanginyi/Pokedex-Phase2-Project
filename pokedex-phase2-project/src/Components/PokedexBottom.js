7ddb0351db8a76fbb9fbe2be4a87c3374babd852
QuangDo <qdangdo@gmail.com>
2021-10-08 05:29:25 -0500

    add horizontal infinity scroll

--- pokedex-phase2-project/src/Components/PokedexBottom.js
+++ pokedex-phase2-project/src/Components/PokedexBottom.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from 'react';
 import PokemonItem from './PokemonItem';
 import SearchBar from './SearchBar';
 import styled from "styled-components";
-import {List, AutoSizer, CellMeasurer, CellMeasurerCache} from "react-virtualized";
+import {FixedSizeList as List} from 'react-window';
+
+
 
 import "../index.css"
 
@@ -24,35 +26,31 @@ function PokedexBottom({setPokeId}) {
 
     const filterPokemon = searchValue === "" ? pokemonData : pokemonData.filter(pokemon => pokemon.name.eng.toLowerCase().includes(searchValue.toLowerCase()));
 
-    // if (filterPokemon.length) {
+    const individualPokemon = ({ index, style, key }) => {
+        const pokemon = filterPokemon[index];
+        return (
+            <div id="poke-list" key={key} style={style}>
+                <PokemonItem id={pokemon.id} name={pokemon.name.eng} setPokeId={setPokeId}/>
+            </div>
+        )
+    };
+    
         return (
             <div>
                 <SearchBar search={search}/>
                 <div>
-                {filterPokemon.length ? 
-                    <PokeScroller style={{width: "100%", height: "52vh"}}>
-                        <AutoSizer>
-                            {({width, height}) => (
+                {filterPokemon.length ?
                             <List
                                 id="poke-list-parent"
-                                width={width}
-                                height={height}
-                                rowHeight={100}
-                                rowCount={filterPokemon.length}
-                                rowRenderer={({key, index, style}) => {
-                                    const pokemon = filterPokemon[index];
-
-                                    return (
-                                        <div id="poke-list" key={key} style={style}>
-                                            <PokemonItem id={pokemon.id} name={pokemon.name.eng} setPokeId={setPokeId}/>
-                                        </div>
-                                    );
-                                }}
-                            />
-                            )}
-                        </AutoSizer>
-                    </PokeScroller> : 
-                    <h1>No Found Data</h1>
+                                width={window.innerWidth}
+                                height={200}
+                                itemCount={filterPokemon.length}
+                                itemSize={450}
+                                layout="horizontal"
+                            >
+                                {individualPokemon}
+                            </List>
+                    :<h1>No Found Data</h1>
                 }
                 </div>
             </div>
